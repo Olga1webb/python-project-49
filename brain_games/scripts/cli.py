19d3fc47baa7b4005a8a743dafd193954397fad6
olga1webb <olga1webb@gmail.com>
2022-12-28 10:03:57 +0300

    Modify cli and script and some other files

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,9 +1,9 @@
 import prompt
 
 def welcome_user():
-
+	"""Welcoming user"""
 	name = prompt.string('May I have your name? ')
 	print(f'Hello, {name}!')
 
 if __name__ == '__main__':
-    welcome_user()
+	welcome_user()
