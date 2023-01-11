f877d90cd957e4ba328fc9e43af9b3e67ed6ce8a
olga1webb <olga1webb@gmail.com>
2023-01-12 01:48:47 +0300

    draft of brain_even script

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,27 @@
+def brain_even_game():
+	number_of_correct_answers = 0
+	number_of_tries = 0
+
+	print('Answer "yes" if the number is even, otherwise answer "no".')
+
+	from random import randint
+
+
+	random_number = randint(0,100)
+	correct_answer = bool(random_number // 2 == 0)
+	answer = prompt.string('Question: ', random_number)
+	
+	print('Your answer: ', answer)
+
+	if (answer == 'yes' and correct_answer == True) or (answer == 'no' and correct_answer == False):
+		number_of_correct_answers += 1
+		print('Correct!')
+
+		if number_of_correct_answers == 3:
+			print(f'Congratulations, {name}!')
+			number_of_correct_answers = 0
+			
+	else:
+		number_of_correct_answers = 0
+		print(answer, ' is wrong answer ;(. Correct answer was \'no\'.')
+		print(f'Let\'s try again, {name}!')
