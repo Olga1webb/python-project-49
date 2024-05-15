bbc2098447dcadcb12e6ded179ffeb54d03d6a5a
olga1webb <olga1webb@gmail.com>
2024-05-15 11:02:46 +0300

    minor improvements on line-beaks in compare modules

--- brain_games/compare.py
+++ brain_games/compare.py
@@ -1,8 +1,8 @@
 def compare_answers(number, user_answer, name, n):
     correct_answer = bool(number % 2 == 0)
 
-    if (correct_answer is True and user_answer == 'yes') or \
-   (correct_answer is False and user_answer == 'no'):
+    if (correct_answer is True and user_answer == 'yes')\
+       or (correct_answer is False and user_answer == 'no'):
         a = 'Correct!'
         n += 1
         print(a)
@@ -11,12 +11,12 @@ def compare_answers(number, user_answer, name, n):
     else:
         n = 0
         if correct_answer is True:
-            a = user_answer 
-                + " is wrong answer ;(. Correct answer was 'yes'\nLet's try again, " 
+            a = user_answer\
+                + " is wrong answer ;(. Correct answer was 'yes'\nLet's try again, "\
                 + name + "!"
         else:
-            a = user_answer 
-                + " is wrong answer ;(. Correct answer was 'no'\nLet's try again, " 
+            a = user_answer\
+                + " is wrong answer ;(. Correct answer was 'no'\nLet's try again, "\
                 + name + "!"
         print(a)    
         return n
--- brain_games/compare_prime.py
+++ brain_games/compare_prime.py
@@ -4,7 +4,7 @@ def compare_answers(number, user_answer, name, n):
         if correct_answer is False:
             break
 
-    if (correct_answer is True and user_answer == 'yes') \
+    if (correct_answer is True and user_answer == 'yes')\
         or (correct_answer is False and user_answer == 'no'):
         a = 'Correct!'
         n += 1
@@ -14,12 +14,12 @@ def compare_answers(number, user_answer, name, n):
     else:
         n = 0
         if correct_answer is True:
-            a = user_answer 
-                + " is wrong answer ;(. Correct answer was 'yes'\nLet's try again, " 
+            a = user_answer\
+                + " is wrong answer ;(. Correct answer was 'yes'\nLet's try again, "\
                 + name + "!"
         else:
-            a = user_answer 
-                + " is wrong answer ;(. Correct answer was 'no'\nLet's try again, " 
+            a = user_answer\
+                + " is wrong answer ;(. Correct answer was 'no'\nLet's try again, "\
                 + name + "!"
         print(a)
         return n
